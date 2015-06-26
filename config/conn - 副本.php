d04f99a5e24fb0f77cbd3f08d7318d3324562d42
dongzhouT <taodongzhou@126.com>
2015-06-26 23:03:21 +0800

    mysql

--- config/conn - 副本.php
+++ config/conn - 副本.php
@@ -1,7 +1,7 @@
 <?php
-$con = mysql_connect("10.4.26.93","upGuJgeTI1fbC","pcJOAtWWmZ8V7") //填写mysql用户名和密码  
+$con = mysql_connect("localhost","root","admin") //填写mysql用户名和密码  
    or die("Could not connect to MySQL server!");  
-   mysql_select_db("dbfa7585ec8554c6387a1447303bc970d") //数据库名  
+   mysql_select_db("msgboard") //数据库名  
    or die("Could not select database!"); 
    mysql_query("set names 'utf8'");
 // echo mysql_error();
